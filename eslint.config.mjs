a1e0fdc4289cb067466dd1572ce5b2837ce92b1d
BeLieVer031 <sandyrajak031@gmail.com>
2024-09-08 10:59:54 +0530

    add preettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,15 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config({
-    extends: [eslint.configs.recommended,
-    ...tseslint.configs.recommendedTypeChecked,
-    ...tseslint.configs.stylisticTypeChecked,],
+    extends: [
+        eslint.configs.recommended,
+        ...tseslint.configs.recommendedTypeChecked,
+        ...tseslint.configs.stylisticTypeChecked,
+        eslintConfigPrettier
+    ],
     languageOptions: {
         parserOptions: {
             projectService: true,
@@ -14,7 +18,5 @@ export default tseslint.config({
         },
     },
     files: ['**/*.ts'],
-    rules: {
-    }
-}
-);
+    rules: {},
+});
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,6 @@
 const x = 45;
 console.log(x);
+{
+  const x = 45;
+  console.log(x);
+}
